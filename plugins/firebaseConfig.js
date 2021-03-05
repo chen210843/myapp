eb05d4c2b925fe67712a68862bc54ff919bccc87
chen210843 <siri.wut210843@gmail.com>
2021-03-05 15:07:28 +0700

    vs1

---
+++ plugins/firebaseConfig.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+// Initialize Cloud Firestore through Firebase
+if (!firebase.apps.length) {
+  const firebaseConfig = {
+    apiKey: 'AIzaSyDiOYmFxy-O46e4c3fIRfv-8TMpyyh0nns',
+    authDomain: 'cs-313-db.firebaseapp.com',
+    projectId: 'cs-313-db',
+    storageBucket: 'cs-313-db.appspot.com',
+    messagingSenderId: '157750849152',
+    appId: '1:157750849152:web:74a0a400a0af76fb2c0065',
+    measurementId: 'G-B00EFNLZDH',
+  }
+  firebase.initializeApp(firebaseConfig)
+}
+
+export const db = firebase.firestore()
